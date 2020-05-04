9a6e4619eb5586a5cda4fed9e879eae184592c0f
AronT <aron.thompson@racunarstvo.hr>
2020-05-04 21:25:46 +0200

    Return Type Delegates & Func

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // create a program that takes in a string and returns the length of it (how many characters)
+    //public delegate int CharacterLength(string text);
+
+    // passing string value, returning int value
+    public Func<string, int> CharacterLength;
+
+    void Start()
+    {
+        CharacterLength = GetCharacters;
+
+        int count = CharacterLength("Johnny");
+        Debug.Log(count);
+    }
+    
+    int GetCharacters(string name)
+    {
+        return name.Length;
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//needed for action
-using System;
 
 public class Player : MonoBehaviour
 {
-    //public delegate void OnDamageReceived(int currentHealth);
-    //public static event OnDamageReceived onDamage;
-
-    // same as above event and its delegate
-    public static Action<int> OnDamageReceived;
-    // void example
-    //public Action onDamage;
-
-
-
-    public int health;
     Input inputActions;
 
     private void Awake()
@@ -25,24 +12,12 @@ public class Player : MonoBehaviour
         inputActions.BasicActions.SpaceActivation.performed += ctx => SpaceActivation_performed();
     }
 
-    private void Start()
-    {
-        health = 10;
-    }
-
     private void SpaceActivation_performed()
     {
-        Damage();
+
     }
 
     private void OnEnable() { inputActions.Enable(); }
 
     private void OnDisable() { inputActions.Disable(); }
-
-    void Damage()
-    {
-        health--;
-
-        OnDamageReceived?.Invoke(health);
-    }
 }
