21a423d77751685a0fbd3804b49174d9bfb82915
AronT <aron.thompson@racunarstvo.hr>
2020-05-04 19:42:37 +0200

    Events

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main.onClick += TurnRed;
+    }
+
+    public void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnDisable()
+    {
+        Main.onClick -= TurnRed;
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,54 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// DELEGATES
+// EVENTS
 public class Main : MonoBehaviour
 {
-    // DEKLARACIJA TIP
-    public delegate void ChangeColor(Color newColor);
-    // DEKLARACIJA VARIJABLE
-    public ChangeColor onColorChange;
+    // DEKLARACIJA TIPA
+    public delegate void ActionClick();
+    // STVARANJE VARIJABLE TIPA EVENT
+    public static event ActionClick onClick;
 
-    // DEKLALRACIJA TIPA
-    public delegate void OnComplete();
-    // DEKLARACIJA VARIJABLE
-    public OnComplete onComplete;
-
-    private void Start()
+    // GETS CALLED FROM GUI BUTTON CLICK
+    public void ButtonClick()
     {
-        // POSTAVLJANJE VARIJABLE
-        onColorChange = UpdateColor;
-        onColorChange(Color.green);
-        // DODAVANJE U VARIJABLU, NESTO SLICNO LISTI
-        onComplete += Task;
-        onComplete += Task2;
-        onComplete += Task3;
-        onComplete -= Task;
-
-        // AKTIVIRANJE VARIJABLE I PROVJERA JE LI NULL
-        onComplete?.Invoke();
-        // SAME AS ABOVE
-        //if (onComplete != null)
+        onClick?.Invoke();
+        // same as above
+        //if (onClick != null)
         //{
-        //    onComplete();   
+        //    onClick();
         //}
-    }
-
-    public void UpdateColor(Color newColor)
-    {
-        Debug.Log("Changing color to: " + newColor.ToString());
-    }
-
-    public void Task()
-    {
-        Debug.Log("Task 1 Finished");
-    }
-    public void Task2()
-    {
-        Debug.Log("Task 2 Finished");
-    }
-    public void Task3()
-    {
-        Debug.Log("Task 3 Finished");
     }
 }
